166b3f17d38c6a2e31d6f5d9229bae57ec3da9df
hacker <awakeningcode@126.com>
2018-07-30 14:40:46 +0800

    【初始化】项目

---
+++ src/main/java/com/myself/je/JeApplication.java
@@ -0,0 +1,12 @@
+package com.myself.je;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeApplication.class, args);
+	}
+}
